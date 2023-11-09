7e1596e7668fbee44766765b9695de75807f8488
miller <mmillermonteiro@gmail.com>
2023-11-09 11:23:56 -0300

    ended aula2

--- Aula2.py
+++ Aula2.py
@@ -1,3 +1,11 @@
 #Variaveis
-idade = 26 
-print(idade)
+idade = 19 
+nome = "Miller"
+altura = 1.79
+estudando = True
+print(type(idade))
+print(type(nome))
+print(type(altura))
+print(type(estudando))
+linguagem = input("Qual linguagem de programação vocÊ está estudando?")
+print("WOW!,", linguagem, "parece ser muito legal")
