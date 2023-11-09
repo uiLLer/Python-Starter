fe978a634a6e77fdc1243f35852c5242c139eeef
miller <mmillermonteiro@gmail.com>
2023-11-09 11:49:04 -0300

    ended aula4

--- aula4.py
+++ aula4.py
@@ -1,5 +1,15 @@
 #conversão de tipos
+"""
+int()
+float()
+str()
+bool()
+"""
 idade = "19"
 print(idade, type(idade))
 idade_int = int(idade)
 print(idade_int)
+
+
+Altura = float(input("informe sua altura ")) #input recebe como string
+print(Altura, type(Altura))
