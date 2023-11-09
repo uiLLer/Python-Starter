6432974a078030ac55a04ac258eefef8546a331d
miller <mmillermonteiro@gmail.com>
2023-11-09 11:33:35 -0300

    created aula3

---
+++ Aula3.py
@@ -0,0 +1,20 @@
+#Operações Matemáticas
+num1 = 10
+num2 = 20
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)     #parte inteira da divisão
+print(20 % 3)           #resto da divisão
+print(2 ** 3)           #potenciação
+
+num3 = 10
+#comparações => resultado booleano
+print(num1 > num2)      
+print(num1 < num2)
+print(num1 == num3)
+print(num1 >= num2)
+print("python" == "Python")     #igual
+print("python" != "Python")     #diferente
